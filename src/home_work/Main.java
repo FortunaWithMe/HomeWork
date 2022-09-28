a82cc8c9ce51a51748cdcf3d11996ecc3699aa09
Andrusenko Dmytro <Dima2mobile@gmail.com>
2022-09-28 22:05:50 +0300

    HomeWork 9

--- src/home_work/Main.java
+++ src/home_work/Main.java
@@ -1,31 +1,25 @@
 package home_work;
-// Текс завдання в README.MD
-
-import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
-
-        Scanner number = new Scanner(System.in);
-        System.out.println("Ведіть число");
-
-        int a;
-        int b;
+        boolean isRunnig = true;
+        int counter =1;
+        int i = 0;
 
 
-        b = (int) (Math.random() *10);
+        while (isRunnig) {
 
-        for (int i = 0; i < 3; i++)
-
-            {
-                a = number.nextInt();
-                if (a == b) {
-                    System.out.println("Ви виграли");
-                    break;
-                } else if (a != b) {
-                    System.out.println("Прошраш");
-                }
+            String shuttleNr = Integer.toString(i);
+            i++;
+            if (shuttleNr.contains("4") || shuttleNr.contains("9")) {
+                continue;
+            }
+            counter ++;
+            System.out.println(shuttleNr);
+            if (counter == 100) {
+                isRunnig = false;
             }
         }
     }
 
+}
